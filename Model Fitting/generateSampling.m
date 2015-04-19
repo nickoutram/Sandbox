2149debe79d94edcf7610657f46c957ca193dfb4
Nicholas Outram <nickoutram@gmail.com>
2015-04-19 23:33:46 +1000

    Minor updates
    Ensured R parameter work, and collapses samples into per reef basis

--- Model Fitting/generateSampling.m
+++ Model Fitting/generateSampling.m
@@ -1,8 +1,8 @@
-function [samplingMatrix] = generateSampling(p, r)
+function [samplingMatrix] = generateSampling(p, R)
 
 % Generate samples from theorised distributions with kernel based larval
 % dispersal
-if nargin==0, p = 8.4444; r = 1; end
+if nargin==0, p = 8.4444; R = 1; end
 
 % generate transport matrix from parameters, and reef stats.
 % do we want to include artificial reefs??? # YES?
@@ -10,6 +10,7 @@ if nargin==0, p = 8.4444; r = 1; end
 load philippinesData.mat % load parentageMatrix, D, Pop_dens & reef_size
 NumReefs = length(D);
 SampledReefs_Juveniles = 1:size(ParentageMatrix,1);
+BINS = [-1 SampledReefs_Juveniles];
 
 k_f = exp(p); % parameterise kernel parameter
 C = exp(-D.^2 ./ k_f); % connectivity matrix
@@ -25,11 +26,15 @@ for thisReef = 1:length(SampledReefs_Juveniles)
 end
 
 % random sample with replacement in each reef
-SampleMatrix = NaN(length(SampledReefs_Juveniles), size(ParentageMatrix,2), r); % empty sample matrix
-for s = 1:length(SampledReefs_Juveniles)
-    SampleMatrix(s, 1:ReefSamples(s), r) = randsample(NumReefs, ReefSamples(s), true, Prop(:,s));
+SampleMatrix = NaN(length(SampledReefs_Juveniles), size(ParentageMatrix,2), R); % empty sample matrix
+
+for r = 1:R % generate R sets of samples over SampledReefs_Juveniles reefs
+    for s = 1:length(SampledReefs_Juveniles)
+        SampleMatrix(s, 1:ReefSamples(s), r) = randsample(NumReefs, ReefSamples(s), true, Prop(:,s));
+    end
 end
 
 SampleMatrix(SampleMatrix > length(SampledReefs_Juveniles)) = -1; % replace unknown reefs with -1
-
+sumSampleMatrix = permute(histc(permute(SampleMatrix, [2 1 3]), BINS), [2 1 3]); % sum number from each reef (sampled at each reef)
+samplingMatrix = sumSampleMatrix;
 end
